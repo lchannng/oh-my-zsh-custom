d0b79a2dfc008a302670d0264de700e0de007c36
fnstanc <fn.stanc@gmail.com>
2018-08-05 01:42:19 +0800

    my omz custom.

---
+++ alias.zsh
@@ -0,0 +1,21 @@
+# editor
+export EDITOR='vim'
+alias vi='vim'
+alias lvim="vim -c \"normal '0\"" #open last edited file
+
+# pacman
+alias pacu='pacman -Syu'
+alias pac='pacman -Si'
+alias pacQi='pacman -Qi'
+alias pacRs='pacman -Rs'
+alias pacs='pacman -Ss'
+
+# extract tarball
+alias gz='tar zxvf'
+alias bz2='tar jxvf'
+
+#others
+alias du='du -h'
+alias df='df -h'
+alias lsblk='lsblk -l -f'
+alias c='clear'
---
+++ env.zsh
@@ -0,0 +1,14 @@
+# Environment variable
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.bin" ] ; then
+    PATH="$HOME/.bin:$PATH"
+fi
+
+if [ -d "$HOME/go" ]; then
+    export GOPATH=$HOME/go
+    export PATH="$PATH:$GOPATH/bin"
+fi
+
+if [ -d "$HOME/usr/bin" ]; then
+    export PATH="$PATH:$HOME/usr/bin"
+fi

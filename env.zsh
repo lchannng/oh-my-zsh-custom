c018fbababa8e30dadb9b6dc08d69a6818155294
lch <l.channng@gmail.com>
2020-04-04 22:11:39 +0800

    update path.

--- env.zsh
+++ env.zsh
@@ -1,14 +1,25 @@
 # Environment variable
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.bin" ] ; then
-    PATH="$HOME/.bin:$PATH"
+if [ -d $HOME/.bin ] ; then
+    PATH=$HOME/.bin:$PATH
 fi
 
-if [ -d "$HOME/coding/go" ]; then
+if [ -d $HOME/.local/bin ]; then
+    export PATH=$PATH:$HOME/.local/bin
+fi
+
+if [ -n $GOPATH ]; then
+    export GOPATH=$HOME/go
+fi
+
+if [ -d $HOME/coding/go ]; then
     export GOPATH=$HOME/coding/go
-    export PATH="$PATH:$GOPATH/bin"
 fi
 
-if [ -d "$HOME/.local/bin" ]; then
-    export PATH="$PATH:$HOME/.local/bin"
+if [ -d $GOPATH/bin ]; then
+    export PATH=$PATH:$GOPATH/bin
+fi
+
+if [ -f $HOME/.private_env.sh ]; then
+    source $HOME/.private_env.sh
 fi

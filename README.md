34fef4ac9bcbd27f2db699d727711dfbbe029235
lchannng <l.channng@gmail.com>
2025-03-11 11:13:57 +0800

    update

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Quick Install
 
 ```bash
-# 使用一键安装命令 (替换为你的仓库地址)
 curl -fsSL https://raw.githubusercontent.com/lchannng/oh-my-zsh-custom/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -8,4 +8,10 @@ git clone https://github.com/lchannng/oh-my-zsh-custom.git ~/.omz
 
 # 修改配置指向子目录
 sed -i 's|^ZSH_THEME=.*|ZSH_THEME="honukai"|' ~/.zshrc
-sed -i "s|^export ZSH=.*|export ZSH=\"\$HOME/.oh-my-zsh\"\nexport ZSH_CUSTOM=\"\$HOME/.omz/custom\"|" ~/.zshrc
+sed -i 's|^export ZSH=.*|export ZSH="$HOME/.oh-my-zsh"|' ~/.zshrc
+
+# 处理两种可能的 ZSH_CUSTOM 情况:
+# 1. 被注释的情况 (# ZSH_CUSTOM=)
+sed -i 's|^# *ZSH_CUSTOM=.*|ZSH_CUSTOM="$HOME/.omz/custom"|' ~/.zshrc
+# 2. 未被注释的情况 (ZSH_CUSTOM=)
+sed -i 's|^ZSH_CUSTOM=.*|ZSH_CUSTOM="$HOME/.omz/custom"|' ~/.zshrc
